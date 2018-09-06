528efe503500b01bdf74e3c18d57342624ea17df
emcd123 <mcdonnell.owen1@gmail.com>
2018-09-06 19:25:34 +0100

    Some structural Changes

--- docs/index.md
+++ docs/index.md
@@ -5,12 +5,19 @@ Title: BookShelf
 
 Documentation
 ========
+##### Table of Contents  
+[Headers](#headers)  
+[Emphasis](#emphasis)  
+...snip...    
+<a name="headers"/>
+## Headers
 
 Overview
 --------
 
 Here are the general instructions on how to run the site, the technologies used and how to submit an issue.
 This github pages site will serve as a thorough manual for the entire site in time. This page is mostly the same as the readme for this repo.
+
 
 Installation
 ------------
@@ -40,4 +47,3 @@ License
 -------
 
 The project is licensed under the BSD license.
-
--- in/mainDash.php
+++ in/mainDash.php
@@ -17,100 +17,19 @@ if($userId === NULL){
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
-<style>
-.error {color: #FF0000;}
-
-.jumbotron-fluid {
-	background-color: grey;
-}
-body {
-    font-family: "Lato", sans-serif;
-}
-
-.sidenav {
-    height: 100%;
-    width: 200px;
-    position: fixed;
-    z-index: 1;
-    top: 52px;
-    left: 0;
-    background-color: #333;
-    overflow-x: hidden;
-    padding-top: 20px;
-}
-
-.sidenav a {
-    padding: 6px 6px 6px 32px;
-    text-decoration: none;
-    font-size: 17px;
-    color: #f2f2f2;
-    display: block;
-}
-
-.sidenav a:hover {
-    color: #f1f1f1;
-}
-
-.main {
-    margin-left: 200px; /* Same as the width of the sidenav */
-}
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
-
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #4CAF50;
-  color: white;
-	text-align: center;
-	width: 200px;
-}
-.topnav a.user {
-  background-color: #333;
-  color: white;
-	text-align: right;
-	width: 800px;
-	right: 0;
-}
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
+<link rel = "stylesheet" type = "text/css" href = "../stylesheets/navigationBars.css" />
 </head>
 <body>
   <div class="topnav">
-    <a class="active" href="mainDash.php">BookShelf</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-		<a class="user" href="#"> <?php echo $userName; ?> </a>
+    <a class="active" href="mainDash.php">HomeShelf</a>
+    <a class="user" href="#"> <?php echo $userName; ?> </a>
 		<button type="button" class="btn btn-success btn-sm" onclick="location.href='../index.php'" style="margin-top: 12px;"> Log Out </button>
   </div>
 	<div class="sidenav">
-		<a href="#">DashBoard</a>
-		<a href="#">Shelves</a>
-		<a href="#">Reading Challenge</a>
+		<a href="mainDash.php">DashBoard</a>
+		<a href="#">Recipes</a>
+		<a href="#">Ingredients</a>
+		<a href="#">Meal Prep</a>
 		<a href="#">Settings</a>
 	</div>
 
