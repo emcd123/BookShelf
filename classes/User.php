8cdaae6d7cf3da7b67841733c5607ab29e73c067
emcd123 <mcdonnell.owen1@gmail.com>
2018-10-08 10:02:10 +0100

    Update User.php
    Fixed bug with object, stupid errors.

--- classes/User.php
+++ classes/User.php
@@ -4,27 +4,30 @@ class User{
   public $userName;
   public $userId;
 
-  /*
-  public function __construct($timestamp, $username, $userId){
-    $this->$timestamp = $timestamp;
-    $this->$userName = $userName;
-    $this->$userId = $userId;
+  public function __construct($ts, $un, $uId){
+    $this->timestamp = $ts;
+    $this->userName = $un;
+    $this->userId = $uId;
   }
   public function __destruct(){
-    $this->$timestamp = $timestamp;
-    $this->$userName = $userName;
-    $this->$userId = $userId;
-  }*/
+    $this->timestamp = $ts;
+    $this->userName = $un;
+    $this->userId = $uId;
+  }
 
   function kick_user(){
     if($this->userId === NULL){
     	header("Location:../index.php");
     	exit;
     }
-  }
+  
 
   function get_name(){
-    return $userName.' UGH';
+    return $this->userName;
   }
 }
+$today = date('Y-m-d');
+$user = new User($today, 'Owen', '1');
+
+echo $user->get_name();
 ?>
